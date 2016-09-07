4b209ef9865925999b2c81451aeccc2f78457cbd
Angus <angustalves@gmail.com>
2016-09-07 03:12:09 +0000

    initial heroku commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var lp = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(lp, function () {
+    console.log('Example app listening on port '+lp+'!');
 });
