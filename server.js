c9075ae4e88d66c848a1ce1566a2c311d84f2a4a
Angus <angustalves@gmail.com>
2016-09-08 02:43:14 +0000

    Display req headers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var lp = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.json(req.headers);
 });
 
 app.listen(lp, function () {
