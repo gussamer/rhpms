58b286d21ddcb4ffb632057a4280f63d4de41f9a
Angus <angustalves@gmail.com>
2016-09-08 03:02:24 +0000

    Added user agent parser middleware

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require('express'), useragent = require('express-useragent');
 var app = express();
 var lp = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.json(req.headers);
+  var ua = useragent.parse(req.headers["user-agent"]);
+  var retobj = {
+    "ipaddress":""
+    ,"langage":req.headers["accept-language"]
+    ,"software":req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  };
+  res.json(ua);
 });
 
 app.listen(lp, function () {
